97e86d8c9b43afa0d15b3d7029b667dab17aaf72
Niraj Amalkanti <neji49@gmail.com>
2015-03-24 00:00:02 -0700

    Added detection functions for main page and multiple choice along with multiple choice submissions

--- pollbot/data/submit.js
+++ pollbot/data/submit.js
@@ -1,6 +1,44 @@
+var instrospectiveLine = "It's an interesting question and one that deserves a lot of thought. I can not do it justice with such a small response";
 
-var clicked = false
-if ($("button").length && !clicked){
-  $("button").click()
+function detectMainPage() {
+    hasPie = false;
+    hasWondering = false;
+    if (3 <= $('div:contains("pie")').length){
+        hasPie = true;
+    }
+    if (3 <= $('div:contains("wondering")').length){
+        hasWondering = true;
+    }
+    return hasPie && hasWondering
 }
-   
+
+function detectMultipleChoice() {
+    if (3 <= $("button").length){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function submitMultipleChoice() {
+  $("button")[0].click();
+}
+
+function submitText() {
+}
+
+if(detectMainPage()){
+    console.log("Found main page");
+}
+
+if (detectMultipleChoice()){
+    console.log("Found multiple choice");
+    submitMultipleChoice();
+}
+
+if (detectTextArea()) {
+    console.log("Found text area");
+    submitText();
+}
+
